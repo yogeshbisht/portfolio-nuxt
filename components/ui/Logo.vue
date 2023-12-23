34b38b2a3414aa6dd203da0c098a21f09671259e
Yogesh Bisht <reinzire@gmail.com>
2023-12-23 22:10:24 +0530

    update logo to hexagonal type

--- components/ui/Logo.vue
+++ components/ui/Logo.vue
@@ -1,9 +1,14 @@
 <template>
-  <div class="flex justify-center items-center gap-2 cursor-pointer" @click="onLogoClick">
-    <NuxtImg src="/logo-light.svg" width="28" height="28" class="md:w-[28px] md:h-[28px] w-[24px] h-[24px]" />
-    <span class="md:text-2xl text-xl font-medium capitalize">
-      YOGESH BISHT
-    </span>
+  <div
+    class="flex justify-center items-center gap-2 cursor-pointer"
+    @click="onLogoClick"
+  >
+    <NuxtImg
+      src="/logo.svg"
+      width="36"
+      height="36"
+      class="md:w-[36px] md:h-[36px] w-[32px] h-[32px]"
+    />
   </div>
 </template>
 
