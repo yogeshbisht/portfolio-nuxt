408994d233dc60a18cf208b3fbc6fff2b2cd4a5d
Yogesh Bisht <reinzire@gmail.com>
2024-07-13 12:55:03 +0530

    update required packages to latest versions

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,21 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
   css: [
     "~/assets/css/main.css",
     "@fortawesome/fontawesome-svg-core/styles.css",
   ],
+
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
+
   modules: ["@nuxt/image", "nuxt-swiper", "@pinia/nuxt", "@nuxt/ui"],
+
   image: {
     provider: "netlify",
     netlify: {
       baseURL: process.env.HOME_URL,
     },
   },
+
+  compatibilityDate: "2024-07-13",
 });
