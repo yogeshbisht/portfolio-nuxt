e77323b3065160bc7389ecb15cb1b121787d4d53
Yogesh Bisht <reinzire@gmail.com>
2024-07-28 21:19:40 +0530

    fix link for personal project portfolio

--- constants/projects.ts
+++ constants/projects.ts
@@ -112,11 +112,14 @@ const projects: ProjectsData[] = [
     id: "portfolio",
     title: "Portfolio Website",
     type: "personal",
-    tech: ["React.js"],
-    intro: ["Portfolio website including some standard animations."],
+    tech: ["React.js", "Framer"],
+    intro: [
+      "Portfolio website including some standard animations.",
+      "The animations are created using Framer Motion.",
+    ],
     imgUrl: "ybnew.png",
     mockupImg: "ybnew.png",
-    webUrl: "https://ybnew.netlify.com/",
+    webUrl: "https://ybnew.netlify.app/",
   },
   {
     id: "homewayz",
